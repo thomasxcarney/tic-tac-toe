21de8c6380227cb8a75d7bc6bb62b055599434c4
thomasxcarney <thomas.carney26@gmail.com>
2023-08-24 15:25:24 -0400

    Add players and ability to change cell content
    There are now two players- player one fills cells with a X and player
    two with O. Still trying to sort out getting the player turn to
    alternate.

--- scripts.js
+++ scripts.js
@@ -5,13 +5,56 @@ const gameBoard = (() => {
         for(let i = 0; i < gameBoardContent.length; i++) {
             let cell = document.createElement('div');
             cell.classList.add(i, 'cell');
+            addListener(cell);
             cell.innerHTML = gameBoardContent[i];
             gameBoardContainer.appendChild(cell);
         };
     };
+
+    const addListener = function(target) {
+        let index = target.classList[0];
+        target.addEventListener('click', event => {
+            fillCell(index);
+            clearGameBoard();
+            renderGameBoard();
+            GameController.playerTurn();
+        })
+    };
+
+    const fillCell = function(index){
+        if(GameController.currentTurn.value === 1) {
+            gameBoardContent[index] = 'X';
+        } else gameBoardContent[index] = 'O';
+    };
+
+    const clearGameBoard = function(){
+        gameBoardContainer.innerHTML = '';
+    };
+
     return {
         renderGameBoard
     };
+})();
+
+const GameController = (() => {
+    const playerCreator = (name, value) => {
+        return {name, value};
+    };
+
+    const playerOne = playerCreator('Player One', 1);
+    const playerTwo = playerCreator('Player Two', 2);
+
+    let currentTurn = playerOne;
+
+    const playerTurn = () => {
+        currentTurn.value === 1 ? currentTurn = playerTwo : currentTurn = playerOne;
+    };
+
+   return{ 
+        currentTurn, 
+        playerTurn
+   };
+    
 })();
 
 gameBoard.renderGameBoard();
