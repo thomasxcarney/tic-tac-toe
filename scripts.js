0b3651ccef0eac859e98eb266ed5008876a6c6cf
thomasxcarney <thomas.carney26@gmail.com>
2023-08-28 16:19:01 -0400

    Make function to play game on start btn click
    The game now runs when the start button is clicked and when the game is
    finished the button allows you to restart.

--- scripts.js
+++ scripts.js
@@ -11,6 +11,13 @@ const gameBoard = (() => {
         };
     };
 
+    const refillContent = function (){
+        gameBoardContent.length = 0;
+        for(let i = 0; i <  9; i++){
+            gameBoardContent.push(' ');
+        };
+    };
+
     const addListener = function(target) {
         let index = target.classList[0];
         target.addEventListener('click', event => {
@@ -35,19 +42,26 @@ const gameBoard = (() => {
         threeInRow(1, 4, 7, marker) || threeInRow(2, 5, 8, marker) || threeInRow(2, 4, 6, marker) || 
         threeInRow(3, 4, 5, marker) || threeInRow(6, 7, 8, marker)) {
             return true;
-        };
+        } else false;
     };
 
     const checkForTie = function() {
-        if(!gameBoardContent.includes(' ')){
+        if(!gameBoardContent.includes(' ') && !checkForThreeInRow('X') && !checkForThreeInRow('O')){
             alert('it\'s a tie');
+            return true;
         };
+    };
+
+    const resetGameBoard = function(){
+        refillContent();
+        clearGameBoard();
     };
 
     return {
         renderGameBoard,
         checkForThreeInRow,
-        checkForTie
+        checkForTie,
+        resetGameBoard
     };
 })();
 
@@ -72,6 +86,9 @@ const GameController = (() => {
             } else object[index] = 'O';
             checkForWin();
             gameBoard.checkForTie();
+            if(gameBoard.checkForTie == true){
+                changeBtnToRestart();
+            }
             playerTurn();
         };
     };
@@ -79,33 +96,43 @@ const GameController = (() => {
     const checkForWin = function() {
         if(gameBoard.checkForThreeInRow('X')){
             alert('Player one wins');
+            changeBtnToRestart();
+            return true;
         } else if(gameBoard.checkForThreeInRow('O')) {
             alert('Player two wins');
+            changeBtnToRestart();
+            return true;
         };
     };
 
     const startBtn = document.getElementById('startBtn');
+
     startBtn.addEventListener('click', event => {
         playGame();
     });
+
+    const changeBtnToRestart = function(){
+        startBtn.innerHTML = 'Restart';
+    }
 
     const player1Name = document.getElementsByClassName('playerName')[0];
     const player2Name = document.getElementsByClassName('playerName')[1];
 
     const setPlayerNames = function () {
-        player1Name.innerHTML = prompt('Enter player one name', ' ');
-        player2Name.innerHTML = prompt('Enter player two name', ' ');
-    }
+        if(player1Name.innerHTML === '' && player2Name.innerHTML === ''){
+            player1Name.innerHTML = prompt('Enter player one name', ' ');
+            player2Name.innerHTML = prompt('Enter player two name', ' ');
+        };
+    };
 
     const playGame = function () {
+        gameBoard.resetGameBoard();
         setPlayerNames();
-    }
+        gameBoard.renderGameBoard();
+    };
 
    return{
         fillCell
    };
     
 })();
-
-
-gameBoard.renderGameBoard();
